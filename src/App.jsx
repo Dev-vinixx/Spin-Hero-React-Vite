2efcd84cdc1885e18d2e023c2042d7e2e4b5ad67
Dev-vinixx <viniciuseduardo0500@gmail.com>
2023-10-14 09:41:51 -0300

    created the spin hero project in 4 hours and sent it for deployment

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/header/Header"
+import Main from "./components/Main/Main"
+import './App.css'
+
+function App() {
+  return (
+    <div className="body">
+
+      <Header />
+
+      <Main />
+
+      <Footer />
+    
+    </div>
+  ) 
+} 
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+function Footer() {
+
+ return (
+
+  <footer>
+
+   <span>made by _vini.x_x &copy; 2023</span>
+
+  </footer>
+
+ )
+
+}
+
+export default Footer
